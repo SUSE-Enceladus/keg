69480e04347886e79c3df5cc40f97204d76ebb7f
Joachim Gleissner <jgleissner@suse.com>
2021-03-16 15:19:04 +0000

    Allow dict keys to be null when merging
    Allow keys to be 'null' when merging into data dictionary. This can be used to
    delete an inherited key.

--- kiwi_keg/utils.py
+++ kiwi_keg/utils.py
@@ -48,6 +48,8 @@ class KegUtils:
             if isinstance(value, dict):
                 node = dest.setdefault(key, {})
                 KegUtils.rmerge(value, node)
+            elif value == None:
+                del dest[key]
             else:
                 dest[key] = value
         return dest
