189b0fd9a2a79253d879cf7c3ec914e2361718ee
Joachim Gleissner <jgleissner@suse.com>
2022-02-04 13:51:12 +0000

    OBS source service: add support for change log generator
    Add support for updating image change logs using keg's change log generator.
    Changes are inserted at the beginning for an existing (flavor.)changes.yaml
    file and namespaced with the image version.
    The latest commit's hash from each repository is stored in _keg_revisions
    (which needs to be stored in the OBS package) and will be used in subsequent
    runs to select the next version's starting point.
    This also changes the version increment implementation to not use kiwi
    to write a new config.kiwi file, as this removes all comments.

--- kiwi_keg/obs_service/compose_kiwi_description.py
+++ kiwi_keg/obs_service/compose_kiwi_description.py
@@ -20,6 +20,8 @@ Usage:
     compose_kiwi_description --git-recipes=<git_clone_source> ... --image-source=<path> --outdir=<obs_out>
         [--git-branch=<name>] ...
         [--disable-version-bump]
+        [--disable-update-changelog]
+        [--disable-update-revisions]
     compose_kiwi_description -h | --help
     compose_kiwi_description --version
 
@@ -40,12 +42,23 @@ Options:
         this option is set.
 
     --disable-version-bump
-        Do not increment the patch version number
+        Do not increment the patch version number.
+
+    --disable-update-changelog
+        Do not update 'changes.yaml'.
+
+    --disable-update-revisions
+        Do not update '_keg_revisions'.
 """
-import os
+import glob
 import docopt
 import itertools
+import logging
+import os
+import re
+import shutil
 import sys
+import tempfile
 
 from kiwi.xml_description import XMLDescription
 from kiwi.utils.temporary import Temporary
@@ -54,6 +67,103 @@ from kiwi.path import Path
 from kiwi_keg.version import __version__
 from kiwi_keg.image_definition import KegImageDefinition
 from kiwi_keg.generator import KegGenerator
+from kiwi_keg.source_info_generator import SourceInfoGenerator
+
+
+log = logging.getLogger('compose_keg_description')
+
+
+def get_kiwi_data(kiwi_config):
+    description = XMLDescription(kiwi_config)
+    return description.load()
+
+
+def get_head_commit_hash(repo_path):
+    result = Command.run(['git', '-C', repo_path, 'show', '--no-patch', '--format=%H', 'HEAD'])
+    return result.output.strip('\n')
+
+
+def get_image_version(kiwi_config):
+    # It is expected that the <version> setting exists only once
+    # in a keg generated image description. KIWI allows for profiled
+    # <preferences> which in theory also allows to distribute the
+    # <version> information between several <preferences> sections
+    # but this would be in general questionable and should not be
+    # be done any case by keg managed recipes. Thus the following
+    # code takes the first version setting it can find and takes
+    # it as the only version information available
+    for preferences in get_kiwi_data(kiwi_config).get_preferences():
+        image_version = preferences.get_version()
+        if image_version:
+            return image_version[0]
+    if not image_version:
+        sys.exit('Cannot determine image version.')
+
+
+def get_revision_args(repo_dirs):
+    rev_args = []
+    if os.path.exists('_keg_revisions'):
+        with open('_keg_revisions') as inf:
+            for line in inf.readlines():
+                rev_spec = line.strip('\n').split(' ')
+                if len(rev_spec) != 2:
+                    sys.exit('Malformed revision spec "{}".'.format(line.strip('\n')))
+                repo_path = repo_dirs.get(rev_spec[0])
+                if not repo_path:
+                    log.warning('Warning: Cannot map URL "{}" to repository.'.format(rev_spec[0]))
+                else:
+                    rev_args += ['-r', '{}:{}..'.format(repo_path.name, rev_spec[1])]
+    else:
+        log.warning(
+            'Warning: no _keg_revision file. '
+            'Change file will contain all applicable commit messages.'
+        )
+    return rev_args
+
+
+def generate_changelog(source_log, outdir, prefix, image_version, rev_args):
+    prefix += '.' if prefix else ''
+    changes_new = os.path.join(outdir, '{}changes.yaml.tmp'.format(prefix))
+    changes_old = '{}changes.yaml'.format(prefix)
+    Command.run([
+        'generate_recipes_changelog',
+        '-o', changes_new,
+        '-f', 'yaml',
+        '-t', image_version,
+        *rev_args,
+        source_log
+    ])
+    if os.path.exists(changes_old):
+        with open(changes_new, 'a') as outf, open(changes_old) as inf:
+            outf.write(inf.read())
+    shutil.move(changes_new, os.path.join(outdir, changes_old))
+
+
+def update_image_version(kiwi_config, version):
+    # Do not use the kiwi export function to update XML data,
+    # as this would remove all comments from the file.
+    new_config = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    with open(kiwi_config, 'r') as inf:
+        exp = re.compile('(\\s*<version>)([^<]*)(</version>.*)')
+        line = inf.readline()
+        while line:
+            m = exp.match(line)
+            if m:
+                new_config.write(m.group(1))
+                new_config.write(version)
+                new_config.write(m.group(3))
+                new_config.write('\n')
+            else:
+                new_config.write(line)
+            line = inf.readline()
+    shutil.move(new_config.name, kiwi_config)
+
+
+def update_revisions(repo_dirs, outdir):
+    with open(os.path.join(outdir, '_keg_revisions'), 'w') as outf:
+        for rname, rpath in repo_dirs.items():
+            chash = get_head_commit_hash(rpath.name)
+            print('{} {}'.format(rname, chash), file=outf)
 
 
 def main() -> None:
@@ -65,18 +175,21 @@ def main() -> None:
     if len(args['--git-branch']) > len(args['--git-recipes']):
         sys.exit('Number of --git-branch arguments must not exceed number of git repos.')
 
-    temp_git_dirs = []
+    handle_changelog = not args['--disable-update-changelog']
+
+    repo_dirs = {}
     for repo, branch in itertools.zip_longest(args['--git-recipes'], args['--git-branch']):
         temp_git_dir = Temporary(prefix='keg_recipes.').new_dir()
         if branch:
             Command.run(['git', 'clone', '-b', branch, repo, temp_git_dir.name])
         else:
             Command.run(['git', 'clone', repo, temp_git_dir.name])
-        temp_git_dirs.append(temp_git_dir.name)
+        repo_dirs[repo] = temp_git_dir
 
     image_definition = KegImageDefinition(
         image_name=args['--image-source'],
-        recipes_roots=temp_git_dirs
+        recipes_roots=[x.name for x in repo_dirs.values()],
+        track_sources=handle_changelog
     )
     image_generator = KegGenerator(
         image_definition=image_definition,
@@ -92,24 +205,36 @@ def main() -> None:
         disable_root_tar=False, overwrite=True
     )
 
+    image_version = None
+    kiwi_config = f'{args["--outdir"]}/config.kiwi'
+
     if not args['--disable-version-bump']:
         # Increment patch version number unless disabled
-        kiwi_config = f'{args["--outdir"]}/config.kiwi'
-        description = XMLDescription(kiwi_config)
-        xml_data = description.load()
-        for preferences in xml_data.get_preferences():
-            # It is expected that the <version> setting exists only once
-            # in a keg generated image description. KIWI allows for profiled
-            # <preferences> which in theory also allows to distribute the
-            # <version> information between several <preferences> sections
-            # but this would be in general questionable and should not be
-            # be done any case by keg managed recipes. Thus the following
-            # code takes the first version setting it can find and takes
-            # it as the only version information available
-            if preferences.get_version():
-                version = preferences.get_version()[0].split('.')
-                version[2] = f'{int(version[2]) + 1}'
-                preferences.set_version(['.'.join(version)])
-                with open(kiwi_config, 'w') as kiwi:
-                    xml_data.export(outfile=kiwi, level=0)
-                break
+        version = get_image_version(kiwi_config)
+        if version:
+            ver_elements = version.split('.')
+            ver_elements[2] = f'{int(ver_elements[2]) + 1}'
+            image_version = '.'.join(ver_elements)
+            update_image_version(kiwi_config, image_version)
+
+    if handle_changelog:
+        sig = SourceInfoGenerator(image_definition, dest_dir=args['--outdir'])
+        sig.write_source_info()
+        rev_args = get_revision_args(repo_dirs)
+
+        if not image_version:
+            log.warning(
+                'Warning: generating changes file but version bump is disabled. '
+                'Using old version.'
+            )
+            image_version = get_image_version(kiwi_config)
+
+        for source_log in glob.glob(os.path.join(args['--outdir'], 'log_sources*')):
+            flavor = source_log[len(os.path.join(args['--outdir'], 'log_sources')) + 1:]
+            generate_changelog(source_log, args['--outdir'], flavor, image_version, rev_args)
+            # clean up source log
+            os.remove(source_log)
+
+    if not args['--disable-update-revisions']:
+        # capture current commits
+        update_revisions(repo_dirs, args['--outdir'])
