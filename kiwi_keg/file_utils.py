01ffe51f92d5fc965a8200203d982a527c9f5454
Joachim Gleissner <jgleissner@suse.com>
2022-05-24 17:13:01 +0100

    update copyright notice year

--- kiwi_keg/file_utils.py
+++ kiwi_keg/file_utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 SUSE Software Solutions Germany GmbH. All rights reserved.
+# Copyright (c) 2022 SUSE Software Solutions Germany GmbH. All rights reserved.
 #
 # This file is part of keg.
 #
