80e7f734006ebce321576a19fed6344254c2bc5e
Joachim Gleissner <jgleissner@suse.com>
2022-11-18 13:03:09 +0000

    Bump version: 2.0.0 → 2.0.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ author = 'public-cloud-dev@susecloud.net'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0.0'
+version = '2.0.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- kiwi_keg/version.py
+++ kiwi_keg/version.py
@@ -18,5 +18,5 @@
 """
 Global version information used in keg and the package
 """
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __githash__ = '$Format:%H$'
