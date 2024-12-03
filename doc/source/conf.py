83502d13bf7face68fe3a2a1d57d7701afec7242
Joachim Gleissner <jgleissner@suse.com>
2024-12-03 15:09:01 +0000

    Avoid Sphinx warning
    Do not set html_theme_path. It is not required in recent Sphinx and this
    avoids a deprecation warning wrt get_html_theme_path().

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -89,8 +89,6 @@ html_sidebars = {
 
 html_theme = "sphinx_rtd_theme"
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 html_theme_options = {
     'collapse_navigation': False,
     'display_version': False
