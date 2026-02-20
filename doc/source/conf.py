99ca96699e2e147c2b281b2c4217909102edaba0
Joachim Gleissner <jgleissner@suse.com>
2026-02-20 12:07:09 +0000

    Bump version: 2.2.0 → 2.2.1

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -48,7 +48,7 @@ author = 'public-cloud-dev@susecloud.net'
 # built documents.
 #
 # The short X.Y version.
-version = '2.2.0'
+version = '2.2.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
